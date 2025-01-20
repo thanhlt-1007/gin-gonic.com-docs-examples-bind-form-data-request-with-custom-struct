24f49023ae65a27c5e05967dca36f21f26a1d839
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-01-20 22:35:36 +0700

    Init gin

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+func main() {
+    router := gin.Default()
+    router.GET("/", func(context *gin.Context) {
+        context.JSON(
+            http.StatusOK,
+            gin.H {
+                "message": "success",
+            },
+        )
+    })
+
+    router.Run(":8080")
+}
