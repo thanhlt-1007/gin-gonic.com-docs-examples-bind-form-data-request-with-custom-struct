3852b617e6e0d4a8e1e8d742077549ecca0138d6
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-01-20 23:10:28 +0700

    Add GET /get-b

--- main.go
+++ main.go
@@ -5,16 +5,40 @@ import (
     "net/http"
 )
 
+type StructA struct {
+    FieldA string `form:"field_a"`
+}
+
+type StructB struct {
+    NestedStruct StructA
+    FieldB string `form:"field_b"`
+}
+
+func getRoot(context *gin.Context) {
+    context.JSON(
+        http.StatusOK,
+        gin.H {
+            "message": "success",
+        },
+    )
+}
+
+func getB(context *gin.Context) {
+    var b StructB
+    context.Bind(&b)
+    context.JSON(
+        http.StatusOK,
+        gin.H{
+            "a": b.NestedStruct,
+            "b": b.FieldB,
+        },
+    )
+}
+
 func main() {
     router := gin.Default()
-    router.GET("/", func(context *gin.Context) {
-        context.JSON(
-            http.StatusOK,
-            gin.H {
-                "message": "success",
-            },
-        )
-    })
+    router.GET("/", getRoot)
+    router.GET("/get-b", getB)
 
     router.Run(":8080")
 }
